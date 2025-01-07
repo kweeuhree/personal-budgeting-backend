b51f4d47a58ad41aade4ad8b784be25b3d278f13
Nika Kolesnikova <firstnamenika@gmail.com>
2025-01-07 09:48:06 -0500

    deploy: troubleshoot renders access to index.html

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -35,6 +35,7 @@ func (app *application) routes() http.Handler {
 	router.GET("/check-index", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		indexPath := filepath.Join(staticDir, "index.html")
 		if _, err := os.Stat(indexPath); os.IsNotExist(err) {
+			log.Printf("Static directory: %s", staticDir)
 			http.Error(w, "index.html not found", http.StatusInternalServerError)
 			return
 		}
